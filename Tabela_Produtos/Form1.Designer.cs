9a3a5d810db43fbfc5928c7c363936c803a9c34f
MatheusAzevedo7 <matheushenrique.mha2@gmail.com>
2024-07-09 20:17:36 -0300

    Adicionar arquivos de projeto.

---
+++ Tabela_Produtos/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Tabela_Produtos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTitulo = new System.Windows.Forms.Label();
+            this.pnTabela = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtTijolo = new System.Windows.Forms.TextBox();
+            this.txtPrego = new System.Windows.Forms.TextBox();
+            this.txtTinta = new System.Windows.Forms.TextBox();
+            this.txtCimento = new System.Windows.Forms.TextBox();
+            this.lbNotaFiscal = new System.Windows.Forms.ListBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.pnTabela.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbTitulo
+            // 
+            this.lbTitulo.AutoSize = true;
+            this.lbTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitulo.ForeColor = System.Drawing.Color.White;
+            this.lbTitulo.Location = new System.Drawing.Point(91, 9);
+            this.lbTitulo.Name = "lbTitulo";
+            this.lbTitulo.Size = new System.Drawing.Size(250, 31);
+            this.lbTitulo.TabIndex = 0;
+            this.lbTitulo.Text = "Tabela de Produtos";
+            // 
+            // pnTabela
+            // 
+            this.pnTabela.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.pnTabela.Controls.Add(this.txtCimento);
+            this.pnTabela.Controls.Add(this.txtTinta);
+            this.pnTabela.Controls.Add(this.txtPrego);
+            this.pnTabela.Controls.Add(this.txtTijolo);
+            this.pnTabela.Controls.Add(this.label11);
+            this.pnTabela.Controls.Add(this.label10);
+            this.pnTabela.Controls.Add(this.label9);
+            this.pnTabela.Controls.Add(this.label2);
+            this.pnTabela.Controls.Add(this.label8);
+            this.pnTabela.Controls.Add(this.label7);
+            this.pnTabela.Controls.Add(this.label6);
+            this.pnTabela.Controls.Add(this.label5);
+            this.pnTabela.Controls.Add(this.label4);
+            this.pnTabela.Controls.Add(this.label3);
+            this.pnTabela.Controls.Add(this.label1);
+            this.pnTabela.Controls.Add(this.lbTitulo);
+            this.pnTabela.Location = new System.Drawing.Point(1, 0);
+            this.pnTabela.Name = "pnTabela";
+            this.pnTabela.Size = new System.Drawing.Size(475, 343);
+            this.pnTabela.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(11, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Produtos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(159, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 31);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Valores";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(295, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 31);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Quantidade";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(11, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 31);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Tijolo";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(11, 281);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 31);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Tinta";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(11, 224);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(115, 31);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Cimento";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(11, 167);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(86, 31);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Prego";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(159, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 31);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "R$ 1,59";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(159, 167);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(109, 31);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "R$ 0,50";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(159, 224);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(124, 31);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "R$ 31,90";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(156, 281);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(139, 31);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "R$ 109,00";
+            // 
+            // txtTijolo
+            // 
+            this.txtTijolo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTijolo.Location = new System.Drawing.Point(327, 106);
+            this.txtTijolo.Name = "txtTijolo";
+            this.txtTijolo.Size = new System.Drawing.Size(100, 38);
+            this.txtTijolo.TabIndex = 13;
+            this.txtTijolo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.QuantidadeProdutos);
+            // 
+            // txtPrego
+            // 
+            this.txtPrego.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrego.Location = new System.Drawing.Point(327, 167);
+            this.txtPrego.Name = "txtPrego";
+            this.txtPrego.Size = new System.Drawing.Size(100, 38);
+            this.txtPrego.TabIndex = 14;
+            this.txtPrego.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.QuantidadeProdutos);
+            // 
+            // txtTinta
+            // 
+            this.txtTinta.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTinta.Location = new System.Drawing.Point(327, 281);
+            this.txtTinta.Name = "txtTinta";
+            this.txtTinta.Size = new System.Drawing.Size(100, 38);
+            this.txtTinta.TabIndex = 15;
+            this.txtTinta.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.QuantidadeProdutos);
+            // 
+            // txtCimento
+            // 
+            this.txtCimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCimento.Location = new System.Drawing.Point(327, 224);
+            this.txtCimento.Name = "txtCimento";
+            this.txtCimento.Size = new System.Drawing.Size(100, 38);
+            this.txtCimento.TabIndex = 16;
+            this.txtCimento.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.QuantidadeProdutos);
+            // 
+            // lbNotaFiscal
+            // 
+            this.lbNotaFiscal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNotaFiscal.FormattingEnabled = true;
+            this.lbNotaFiscal.ItemHeight = 24;
+            this.lbNotaFiscal.Location = new System.Drawing.Point(496, 9);
+            this.lbNotaFiscal.Name = "lbNotaFiscal";
+            this.lbNotaFiscal.Size = new System.Drawing.Size(326, 268);
+            this.lbNotaFiscal.TabIndex = 2;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.Color.White;
+            this.btnCalcular.Location = new System.Drawing.Point(604, 294);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(131, 39);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Comprar";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(834, 345);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lbNotaFiscal);
+            this.Controls.Add(this.pnTabela);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmTabela";
+            this.pnTabela.ResumeLayout(false);
+            this.pnTabela.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTitulo;
+        private System.Windows.Forms.Panel pnTabela;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTijolo;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCimento;
+        private System.Windows.Forms.TextBox txtTinta;
+        private System.Windows.Forms.TextBox txtPrego;
+        private System.Windows.Forms.ListBox lbNotaFiscal;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Tabela_Produtos/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tabela_Produtos
+{
+    public partial class Form1 : Form
+    {
+        double quantidade = 0;
+        double totalCompra = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            // Adiciona o evento Click para todas as TextBoxes
+            txtTijolo.Click += LimparListBox;
+            txtPrego.Click += LimparListBox;
+            txtCimento.Click += LimparListBox;
+            txtTinta.Click += LimparListBox;
+
+            // Adiciona o evento Click para o TextBox lbNotaFiscal
+            lbNotaFiscal.Click += LimparListBox;
+        }
+
+        private void LimparListBox(object sender, EventArgs e)
+        {
+            lbNotaFiscal.Items.Clear();
+        }
+        private void QuantidadeProdutos(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            lbNotaFiscal.Items.Clear();
+
+            if (string.IsNullOrWhiteSpace(txtTijolo.Text) &&
+            string.IsNullOrWhiteSpace(txtPrego.Text) &&
+            string.IsNullOrWhiteSpace(txtCimento.Text) &&
+            string.IsNullOrWhiteSpace(txtTinta.Text))
+            {
+                MessageBox.Show("Preencha pelo menos um dos campos de quantidade antes de realizar a compra.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; // Sai do método sem fazer mais nada
+            }
+            totalCompra = 0;
+
+            if (double.TryParse(txtTijolo.Text, out quantidade))
+            {
+                double precoUnitario = 1.59;
+                double resultado = quantidade * precoUnitario;
+                lbNotaFiscal.Items.Add($"Valor total do(s) Tijolo(s): R$ {resultado.ToString("F2")}");
+                totalCompra += resultado;
+                txtTijolo.ResetText();
+            }
+
+            if (double.TryParse(txtPrego.Text, out quantidade))
+            {
+                double precoUnitario = 0.50;
+                double resultado = quantidade * precoUnitario;
+                lbNotaFiscal.Items.Add($"Valor total do(s) Prego(s): R$ {resultado.ToString("F2")}");
+                totalCompra += resultado;
+                txtPrego.ResetText();
+            }
+
+            if (double.TryParse(txtCimento.Text, out quantidade))
+            {
+                double precoUnitario = 31.90;
+                double resultado = quantidade * precoUnitario;
+                lbNotaFiscal.Items.Add($"Valor total do(s) Cimento(s): R$ {resultado.ToString("F2")}");
+                totalCompra += resultado;
+                txtCimento.ResetText();
+            }
+
+            if (double.TryParse(txtTinta.Text, out quantidade))
+            {
+                double precoUnitario = 109.00;
+                double resultado = quantidade * precoUnitario;
+                lbNotaFiscal.Items.Add($"Valor total da(s) Tinta(s): R$ {resultado.ToString("F2")}");
+                totalCompra += resultado;
+                txtTinta.ResetText();
+            }
+
+            lbNotaFiscal.Items.Add($"Total da compra foi: R$ {totalCompra.ToString()}");
+
+        }
+    }
+}
